8d4fe14528b9e9606410b156f8cf6dc6bfb8ba18
devadil <haddadadil01@hotmail.fr>
2016-11-07 15:38:43 +0100

    boostrap , enabled bluprint

--- api/controllers/CustumerController.js
+++ api/controllers/CustumerController.js
@@ -6,6 +6,56 @@
  */
 
 module.exports = {
-	
+
+
+
+  'new' : function (req, res) {
+
+    res.view() ;
+
+  },
+
+
+  'create' : function (req , res , next) {
+
+    Custumer.create(req.params.all() , function (err , custumer) {
+
+      if (err) return next (err) ;
+
+      res.redirect('/custumer/show/' +  custumer.id) ;
+
+    });
+
+  } ,
+
+
+  'show' : function (req , res , next) {
+
+    Custumer.findOne(req.param('id') ,  function (err , custumer) {
+
+      if (err) return next()  ;
+      if (!custumer) return next() ;
+
+      res.view({
+
+        custumer :  custumer
+
+      });
+
+    })
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
